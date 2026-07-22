7099f4d601a89c56ed7d227968339dcc81b49b23
jordelmir <jordelmir@gmail.com>
2026-07-22 08:25:20 -0600

    fix: Add NSApp foreground activation so GUI window immediately pops up on launch

--- Sources/ElysiumApp/main.swift
+++ Sources/ElysiumApp/main.swift
@@ -1,14 +1,26 @@
 import SwiftUI
+import AppKit
 import ElysiumCore
 import ElysiumUI
 
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.activate(ignoringOtherApps: true)
+        if let window = NSApp.windows.first {
+            window.makeKeyAndOrderFront(nil)
+            window.center()
+        }
+    }
+}
+
 @main
 struct ElysiumVanguardApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup("WinMac Elysium Vanguard") {
             GameLibraryView()
-                .frame(minWidth: 900, minHeight: 650)
+                .frame(minWidth: 1000, minHeight: 700)
         }
-        .windowStyle(.hiddenTitleBar)
     }
 }
